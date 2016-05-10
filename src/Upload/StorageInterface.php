6d43ff4e9d0b9649ef8f70596444cd7923bdf13d
Matt Trask <mjftrask@gmail.com>
2016-05-09 21:06:33 -0400

    fixed use statements in interface

--- src/Upload/StorageInterface.php
+++ src/Upload/StorageInterface.php
@@ -30,6 +30,7 @@
  */
 namespace Upload;
 
+use \Upload\FileInfoInterface;
 /**
  * Storage Interface
  *
@@ -45,8 +46,8 @@ interface StorageInterface
      * This method is responsible for uploading an `\Upload\FileInfoInterface` instance
      * to its intended destination. If upload fails, an exception should be thrown.
      *
-     * @param  \Upload\FileInfoInterface $fileInfo
+     * @param  FileInfoInterface $fileInfo
      * @throws \Exception                If upload fails
      */
-    public function upload(\Upload\FileInfoInterface $fileInfo);
+    public function upload(FileInfoInterface $fileInfo);
 }
